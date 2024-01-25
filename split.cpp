36400d10a80de7dbfc6e38ebffe21cb3a84f5cbc
Cassandra Wong <cywong@usc.edu>
2024-01-25 08:49:09 +0000

    infinite while loop issue :(

--- split.cpp
+++ split.cpp
@@ -11,21 +11,80 @@ the function below should be the only one in this file.
 */
 
 #include "split.h"
-
+#include <iostream>
+using namespace std;
 /* Add a prototype for a helper function here if you need */
-
+//passing in, odds, and evens by the most recent node after subsequent passes since we arent returning anything
 void split(Node*& in, Node*& odds, Node*& evens)
 {
   /* Add code here */
 // WRITE YOUR CODE HERE
   //pseudo code 
-  // BASE CASE check if in's val is null; if not;
+  // BASE CASE when in's val is null, then its empty
+
+  cout << "currently on node val" << in->value << endl;
+  if(in == nullptr){
+    //done dont do anything
+    cout << "done!" << endl;
+  }
+  else {
+    //else  check if even or odd then link it
+  //update in
+  //recursively call again
+  int temp = in->value;
+  Node* holder = nullptr;
+  if (temp % 2 == 0){
+    //CASE 1: EVEN
+    cout << "checking if even" << endl;
+    //check if evens is empty
+    if(evens == nullptr){
+      evens = in;
+      in = in->next;
+      
+    }
+    else {
+      //do while loop to get to the last node
+      holder = evens;     
+      while(holder->next != nullptr){
+        cout << "holder value " << holder->value << endl;
+        holder = holder->next;
+        
+      }
+      holder->next = in;
+      in = in->next;
+    }
+    in = in->next;
+    split(in, odds, evens);
+  }
+  else {
+      //CASE 2 ODD
+      cout << "checking if odd" << endl;
+      if(odds == nullptr){
+        odds = in;
+        in = in->next;
+        cout << "60" << endl;
+        
+      }
+      else {
+        //do while loop to get to the last node
+        holder = odds;
+        cout << "66" << endl;
+        while(holder->next!= nullptr){
+          holder = holder->next;
+        }
+        holder->next = in;
+        in = in->next;
+      }
+      cout << "fin odd, now calling recursion" << endl;
+      split(in, odds, evens);
+    }
+    
+  }
+  
+  }
 
   
-  //else  check if even or odd then link it
-  //remove it from in 
-  //recursively call again
 
-}
+
 
 /* If you needed a helper function, write it here */
--- test_split.cpp
+++ test_split.cpp
@@ -10,8 +10,77 @@ g++ split.cpp test_split.cpp -o test_split
 */
 
 #include "split.h"
-
-int main(int argc, char* argv[])
+#include <iostream>
+using namespace std;
+int main() // int main (int argc, char* argv[])
 {
+  int A [10] = {1,2,3,4,5,6,7,8,9,10};
+  Node* nArray [10];
+  Node* temp = nullptr, *in = nullptr, *evens = nullptr, *odds = nullptr;
+  
+  //just making a bunch of nodes
+  for (int i = 0; i < 10; i++){
+    temp = new Node(A[i],nullptr);
+    nArray[i] = temp;
+  }
+  cout << nArray[8]->value << " value, " << nArray[8]->next << endl;
+  cout << nArray[9]->value << " value, " << nArray[9]->next << endl;
+  //now to link the nodes, leave last node alone
+  for(int i = 0; i < 9; i++){
+    if(i == 0) {
+      //this is the first node, need to make in point to it
+      nArray[0]->next = nArray[1];
+      in = nArray[0];
+    }
+    else {
+      
+        nArray[i]->next = nArray[i+1];
 
+    }
+    
+  }
+
+  //PRINTING OUT "IN"
+    cout << "Printing IN ARRAY " << endl;
+    int cntr = 1;
+    Node* curr = in;
+    while (curr != nullptr){
+      cout << "node " << cntr << " value = " << curr->value << endl;
+      cntr ++;
+      curr = curr->next;
+    }
+
+  //CALLING split
+  cout <<"calling split" << endl;
+  split(in, odds, evens);
+  //PRINTING OUT "IN"
+    cout << "Printing IN ARRAY post split " << endl;
+     cntr = 1;
+    curr = in;
+    while (curr != nullptr){
+      cout << "node " << cntr << " value = " << curr->value << endl;
+      cntr ++;
+      curr = curr->next;
+    }
+
+    //PRINTING OUT "evens"
+    cout << "Printing evens ARRAY post split " << endl;
+     cntr = 1;
+     curr = evens;
+    while (curr != nullptr){
+      cout << "node " << cntr << " value = " << curr->value << endl;
+      cntr ++;
+      curr = curr->next;
+    }
+    //PRINTING OUT "odds"
+    cout << "Printing odds ARRAY post split " << endl;
+     cntr = 1;
+     curr = odds;
+    while (curr != nullptr){
+      cout << "node " << cntr << " value = " << curr->value << endl;
+      cntr ++;
+      curr = curr->next;
+    }
+
+  return 0;
 }
